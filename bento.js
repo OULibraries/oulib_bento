4bff4844b4675945d9c099acbd464c841e5faac3
Tim <timsmith@ou.edu>
2016-12-06 14:44:11 -0600

    add more configuration options for oneSearch

--- bento.js
+++ bento.js
@@ -19,9 +19,10 @@ function displayResults(target, json){
 // run a search against the gateway
 function doSearch(target, needle) {
     console.log( needle, target);
-    var searchURI = Drupal.settings.oulib_bento.uri;
+    var searchURI = Drupal.settings.oulib_bento.searchURI;
+    var resultLimit = Drupal.settings.oulib_bento.resultLimit;
 
-    var myurl=searchURI + "/search?t="+target+"&q="+needle+"&n=10";
+    var myurl=searchURI + "/search?t="+target+"&q="+needle+"&n=" + resultLimit;
     $.ajax({
 	url: myurl,
 	dataType: "jsonp",
