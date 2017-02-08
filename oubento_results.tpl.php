e88f44ee6bb1a86301624a3a58ff0f2d5f5b32f0
c-corbett <ccorbett@ou.edu>
2017-02-08 13:26:04 -0600

    Adjusting hover text for icons.

--- oubento_results.tpl.php
+++ oubento_results.tpl.php
@@ -14,23 +14,23 @@ if ($silo == 'extrasearch') { ?>
       </p>
       <a class="col-md-2 extra-search" id="search-scholar-icon"
          href="https://scholar.google.com"><img
-          src="/sites/all/themes/oulib_bootstrap/img/icon_google_scholar.png" title="Google Scholar"></a>
+          src="/sites/all/themes/oulib_bootstrap/img/icon_google_scholar.png" title="Google Scholar, search scholarly literature from a variety of disciplines." alt="Google Scholar"></a>
 
       <a class="col-md-2 extra-search" id="search-threed-icon"
          href="https://sketchfab.com"><img
-          src="/sites/all/themes/oulib_bootstrap/img/icon_3D_models.png" title="3D Models in Sketchfab"></a>
+          src="/sites/all/themes/oulib_bootstrap/img/icon_3D_models.png" title="Sketchfab, Search for 3D models." alt="Sketchfab"></a>
 
       <a class="col-md-2 extra-search" id="search-worldcat-icon"
          href="https://ou.worldcat.org"><img
-          src="/sites/all/themes/oulib_bootstrap/img/icon_worldcat.png" title="OCLC WorldCat"></a>
+          src="/sites/all/themes/oulib_bootstrap/img/icon_worldcat.png" title="OCLC WorldCat, Search thousands of libraries around the world." alt="OCLC Worldcat"></a>
 
       <a class="col-md-2 extra-search" id="search-dpla-icon"
          href="https://dp.la"><img
-          src="/sites/all/themes/oulib_bootstrap/img/icon_dpla.png" title="Digital Public Library of America"></a>
+          src="/sites/all/themes/oulib_bootstrap/img/icon_dpla.png" title="Digital Public Library of America, Search America’s galleries, libraries, archives, and museums." alt="Digital Public Library of America"></a>
 
       <a class="col-md-2 extra-search" id="search-hathitrust-icon"
          href="https://babel.hathitrust.org"><img
-          src="/sites/all/themes/oulib_bootstrap/img/icon_hathitrust.png" title="HathiTrust"></a>
+          src="/sites/all/themes/oulib_bootstrap/img/icon_hathitrust.png" title="HathiTrust, Search digitized content from research libraries." alt="HathiTrust"></a>
 		  
     </div>
   </div>
