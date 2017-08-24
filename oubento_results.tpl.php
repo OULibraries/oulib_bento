9f17429dc85de4d7b3d339afa117aed92b58dbad
Logan Cox <lmc@ou.edu>
2017-08-24 12:02:25 -0500

    Open other searches in new window/tab for consistency.

--- oubento_results.tpl.php
+++ oubento_results.tpl.php
@@ -12,26 +12,32 @@ if ($silo == 'extrasearch') { ?>
         Perform this search on another platform (will leave libraries website).
       </p>
       <a class="col-md-2 extra-search" id="search-scholar-icon" rel="ext"
+         target="_blank"		 
          href="https://scholar.google.com"><img
           src="/sites/all/themes/oulib_bootstrap/img/icon_google_scholar.png" title="Google Scholar: Search scholarly literature from a variety of disciplines." alt="Google Scholar"></a>
 
       <a class="col-md-2 extra-search" id="search-threed-icon" rel="ext"
+         target="_blank"
          href="https://sketchfab.com"><img
           src="/sites/all/themes/oulib_bootstrap/img/icon_3D_models.png" title="Sketchfab: Search for 3D models." alt="Sketchfab"></a>
 
       <a class="col-md-2 extra-search" id="search-worldcat-icon" rel="ext"
+         target="_blank"
          href="https://ou.worldcat.org"><img
           src="/sites/all/themes/oulib_bootstrap/img/icon_worldcat.png" title="OCLC WorldCat: Search thousands of libraries around the world." alt="OCLC Worldcat"></a>
 
       <a class="col-md-2 extra-search" id="search-dpla-icon" rel="ext"
+         target="_blank"
          href="https://dp.la"><img
           src="/sites/all/themes/oulib_bootstrap/img/icon_dpla.png" title="Digital Public Library of America: Search America’s galleries, libraries, archives, and museums." alt="Digital Public Library of America"></a>
 
       <a class="col-md-2 extra-search" id="search-hathitrust-icon" rel="ext"
+         target="_blank"
          href="https://babel.hathitrust.org"><img
           src="/sites/all/themes/oulib_bootstrap/img/icon_hathitrust.png" title="HathiTrust: Search digitized content from research libraries." alt="HathiTrust"></a>
 		  
       <a class="col-md-2 extra-search" id="search-opentextbooks-icon" rel="ext"
+         target="_blank"
          href="http://open.umn.edu/opentextbooks/"><img
           src="/sites/all/modules/oulib_bento/img/icon_otl.png" title="Open Textbook Library: Search for open-licensed peer-reviewed textbooks." alt="Open Textbook Library"></a>
     </div>
